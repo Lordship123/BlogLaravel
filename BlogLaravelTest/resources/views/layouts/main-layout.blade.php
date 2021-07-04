fdc96423e1c01352bc20a9290442c68f1ca48f1e
Lordship123 <egor87768@gmail.com>
2021-07-05 00:13:09 +0200

    Updated Modified NavBar

--- BlogLaravelTest/resources/views/layouts/main-layout.blade.php
+++ BlogLaravelTest/resources/views/layouts/main-layout.blade.php
@@ -10,27 +10,39 @@
 </head>
 <body>
 @section('admin_navbar')
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">MyBlog</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/">Home <span class="sr-only"></span></a>
-                    </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/private/@yield('href')">@yield('second-nav-item')</a>
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/logout">Logout</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Sign in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/private/@yield('href')">@yield('second-nav-item')</a>
+                    </li>
                 </ul>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
 @show
 <div class="container blog_home_container">
     <h1 class="mt-5 mb-4 text-center">@yield('title')</h1>
     @yield('content')
 </div>
+<script>
+    $(document).on('click','.navbar-collapse.show',function(e) {
+        $(this).collapse('hide');
+    });
+</script>
 </body>
 </html>
